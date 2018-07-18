04d458dfdd241d26f7fdb4104eb8c5d714f01497
Bestun Gardi <bestun95@yahoo.com>
2018-07-18 02:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks (blocknumber) 
-{
-  if (blocknumber > 42)
-  {
-    return blocknumber - 42;
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
   }
- else {
-   return 42- blocknumber;
- }
-  
 }
 
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
 
-function distanceFromHqInFeet (feet) 
-{
-  if(feet <= 43&& feet >34)
-  {
-  return feet + 221;
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
   }
-  
-  else if(feet>=43)
-  {
-    return feet + 2062;
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
-  
-  else 
-  {
-    return feet+2078;
-  }
-  
 }
